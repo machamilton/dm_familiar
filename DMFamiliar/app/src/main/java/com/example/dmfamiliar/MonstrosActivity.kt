8a3a8d6b14c3ffb14383d21a680601ce793a7bfc
gmachamilton <machamilton16@gmail.com>
2020-11-28 23:31:43 -0300

    Feita a tela de magias

--- DMFamiliar/app/src/main/java/com/example/dmfamiliar/MonstrosActivity.kt
+++ DMFamiliar/app/src/main/java/com/example/dmfamiliar/MonstrosActivity.kt
@@ -12,23 +12,26 @@ class MonstrosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monstros)
+        //Traz os dados da activity BuscaActivity
         val titulo = intent.getStringExtra("textoTitulo")
         val conteudo = intent.getStringExtra("conteudo")
         val tituloPagina = findViewById<TextView>(R.id.titulo)
+        //Define o titulo da pagina como a busca realizada
         if (titulo != null) {
             tituloPagina.setText(titulo.capitalize())
         }
         val infoView = findViewById<TextView>(R.id.textView2)
+        //Variavel objeto contem os dados da requisicao realizada
         val objeto = JSONObject(conteudo)
         val nome = "Nome: " + objeto.getString("name")
         val tamanho: String = "Tamanho: " + objeto.getString("size")
         val tipo = "Tipo: " + objeto.getString("type")
         val subtipo = "SubTipo: " + objeto.getString("subtype")
         val alinhamento = "Alinhamento: " + objeto.getString("alignment")
-
+        //Retorna as informacoes basicas do monstro
         val informacoes: String = nome + "\n" + tamanho + "\n" + tipo + "\n" + subtipo + "\n" + alinhamento + "\n"
         infoView.setText(informacoes)
-
+        //Retorna as informacoes de combate do monstro
         val tituloCombate = findViewById<TextView>(R.id.textView4)
         tituloCombate.setText("Informações de Combate")
 
@@ -37,8 +40,19 @@ class MonstrosActivity : AppCompatActivity() {
         val armadura = "Pontos de Armadura: " + objeto.getString("armor_class")
         val hit_dice = "Dados de Vida: " + objeto.getString("hit_dice")
 
-        val informacoesCombate = hp + "\n" + armadura + "\n" + hit_dice
+        val informacoesCombate = "\n" + hp + "\n" + armadura + "\n" + hit_dice
         infoCombate.setText(informacoesCombate)
+
+        val infoAtributos = findViewById<TextView>(R.id.atributos)
+        val forca = "Força: " + objeto.getString("strength")
+        val destreza = "Destreza: " + objeto.getString("dexterity")
+        val const = "Constituição: " + objeto.getString("constitution")
+        val int = "Inteligência: " + objeto.getString("intelligence")
+        val sabedoria = "Sabedoria: " + objeto.getString("wisdom")
+        val carisma = "Carisma: " + objeto.getString("charisma")
+
+        val retornoAtributos = "\n" + forca + "\n" + destreza + "\n" + const + "\n" + int + "\n" + sabedoria + "\n" + carisma
+        infoAtributos.setText(retornoAtributos)
 
 
 
--- DMFamiliar/app/src/main/java/com/example/dmfamiliar/SpellsActivity.kt
+++ DMFamiliar/app/src/main/java/com/example/dmfamiliar/SpellsActivity.kt
@@ -2,10 +2,33 @@ package com.example.dmfamiliar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.json.JSONObject
 
 class SpellsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spells)
+        val titulo = intent.getStringExtra("textoTitulo")
+        val conteudo = intent.getStringExtra("conteudo")
+        val tituloPagina = findViewById<TextView>(R.id.tituloSpell)
+        //Define o titulo da pagina como a busca realizada
+        if (titulo != null) {
+            tituloPagina.setText(titulo.capitalize())
+        }
+        val infoView = findViewById<TextView>(R.id.conteudoSpell)
+        //Variavel objeto contem os dados da requisicao realizada
+        val objeto = JSONObject(conteudo)
+        val nome = "Nome: " + objeto.getString("name")
+        val desc = "Descrição: " + objeto.getString("desc")
+        val hi_lv = "Níveis mais altos: " + objeto.getString("higher_level")
+        val range = "Alcance da magia: " + objeto.getString("range")
+        val ritual = "É um ritual? "  +  objeto.getString("ritual")
+        val concentration = "Precisa de concentração? " + objeto.getString("concentration")
+        val duracao = "Duração: " + objeto.getString("duration")
+        val casting_time = "Tempo de Invocação: " + objeto.getString("casting_time")
+        val level = "Level: " + objeto.getString("level")
+        val infototal = nome + "\n" + desc + "\n" + hi_lv + "\n" + range + "\n" + ritual + "\n" + concentration + "\n" + duracao + "\n" + casting_time + "\n" + level
+        infoView.setText(infototal)
     }
 }
